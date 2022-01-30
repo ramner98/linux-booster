b3a4c21fd4092f6194230220fca2f8c94cb2e1c6
ramner98 <98687653+ramner98@users.noreply.github.com>
2022-01-30 04:49:39 -0500

    Delete linux-aos directory

--- linux-aos/scripts/ananicy-install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-
-git clone https://github.com/Nefelim4ag/Ananicy.git
-./Ananicy/package.sh debian
-sudo dpkg -i ./Ananicy/ananicy-*.deb
-sudo systemctl enable ananicy
-sudo systemctl start ananicy
--- linux-aos/scripts/perlockd_install.sh
+++
@@ -1,8 +0,0 @@
-#!bin/bash
-
-cd prelockd-master
-sudo make install
-sudo systemctl enable --now prelockd.service
-sudo systemctl restart  prelockd
-
-cd ..
