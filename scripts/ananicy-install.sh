8d5318bf2f797a23bba892f0e2345aa284a6eed8
ramner98 <98687653+ramner98@users.noreply.github.com>
2022-02-15 11:16:36 -0500

    Update ananicy-install.sh

--- scripts/ananicy-install.sh
+++ scripts/ananicy-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
-git clone https://github.com/Nefelim4ag/Ananicy.git
-./Ananicy/package.sh debian
-sudo dpkg -i ./Ananicy/ananicy-*.deb
+git clone https://github.com/Nefelim4ag/Ananicy.git /tmp/ananicy
+cd /tmp/ananicy
+sudo make install
 sudo systemctl enable ananicy
 sudo systemctl start ananicy
