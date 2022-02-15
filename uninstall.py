2b5fad0dcc5b8ab1cf36b3e88f6b7b065d0f1c65
ramner98 <98687653+ramner98@users.noreply.github.com>
2022-02-15 11:20:48 -0500

    Update uninstall.py

--- uninstall.py
+++ uninstall.py
@@ -9,6 +9,42 @@ def unistallingaos_commands():
 ############################################################################################################################
 
 
+#########################################
+# Prepare the system for uninstallation #
+#########################################
+
+  def prepare_un_commands():
+    pro = subprocess.run(['sudo', 'bash', 'scripts/fix.sh'])
+
+    print(pro.returncode)
+
+    if int(pro.returncode)==0:
+        print("#######################################################") 
+        print("*             Preparation was successful              *")
+        print("#######################################################")
+        print("")
+        print("the program will continue the installation process in a few seconds, please wait ...")
+        time.sleep(3)
+
+    else:
+
+        print("############################################################################") 
+        print("*                   warning: Preparation was was failed                    *")
+        print("############################################################################")
+        time.sleep(3)
+        print("")
+        loop = input("Do you want to try to Prepare the system to uninstallation again? [y/n]")  
+        if loop  == "y":
+          prepare_un_commands()
+          
+
+  prepare_un_commands()
+
+
+##############################################################################################################################
+##############################################################################################################################
+
+
 ####################
 # linux-aos-backup #
 ####################
@@ -25,7 +61,7 @@ def unistallingaos_commands():
           print("")
           print("")
           print("the program will continue the installation process in a few seconds, please wait ...")
-          time.sleep(5)
+          time.sleep(3)
 
       else:
 
@@ -36,7 +72,9 @@ def unistallingaos_commands():
           print("")
           loop = input("Do you want to try to enable linux-aos-backup again? [y/n]")  
           if loop  == "y":
+            subprocess.run(['sudo', 'bash', 'scripts/fix.sh'])
             timeshift_commands()
+            
 
                   
   timeshift_commands()
@@ -52,29 +90,29 @@ def unistallingaos_commands():
 
 
   def unistalling_commands():
-    pro2 = subprocess.run(['sudo', 'apt-get', 'remove', 'preload', '-y'])
-    pro3 = subprocess.run(['sudo', 'apt-get', 'remove', 'prelink', '-y'])
-    pro4 = subprocess.run(['sudo', 'apt-get', 'remove', 'gnome-disk-utility', '-y'])
-    pro5 = subprocess.run(['sudo', 'apt-get', 'remove', 'nohang', '-y'])
-    pro6 = subprocess.run(['sudo', 'apt-get', 'remove', 'fish', '-y'])
-    pro7 = subprocess.run(['sudo', 'apt-get', 'remove', 'mesa-utils', '-y'])
-    pro8 = subprocess.run(['sudo', 'apt-get', 'remove', 'mesa-utils-extra', '-y'])
-    pro9 = subprocess.run(['sudo', 'apt-get', 'remove', 'indicator-cpufreq', '-y'])
-    pro10 = subprocess.run(['sudo', 'apt-get', 'remove', 'profile-sync-daemon', '-y'])
-    pro11 = subprocess.run(['sudo', 'apt-get', 'remove', 'fakeroot', '-y'])
-    pro12 = subprocess.run(['sudo', 'systemctl', 'disable', '--now', 'prelockd.service'])
-    pro13 = subprocess.run(['sudo', 'apt-get', 'remove', 'zram-config', '-y'])
-    pro14 = subprocess.run(['sudo', 'apt-get', 'remove', 'tlp', '-y'])
-    pro15 = subprocess.run(['sudo', 'apt-get', 'remove', 'tlpui', '-y'])
-    pro16 = subprocess.run(['sudo', 'systemctl', 'disable', '--now', 'memavaild.service'])
-    pro17 = subprocess.run(['sudo', 'apt-get', 'remove', 'tuned', 'tuned-utils', 'tuned-utils-systemtap', '-y'])
-    pro18 = subprocess.run(['sudo', 'apt-get', 'remove', 'fail2ban', '-y'])
-    pro19 = subprocess.run(['sudo', 'ufw', 'disable'])
-    pro20 = subprocess.run(['sudo', 'apt-get', 'remove', 'git', '-y'])
-    pro21 = subprocess.run(['sudo', 'systemctl', 'disable', 'ananicy'])
+    pro = subprocess.run(['sudo', 'apt-get', 'remove', 'preload', '-y'])
+    pro2 = subprocess.run(['sudo', 'apt-get', 'remove', 'prelink', '-y'])
+    pro3 = subprocess.run(['sudo', 'apt-get', 'remove', 'gnome-disk-utility', '-y'])
+    pro4 = subprocess.run(['sudo', 'apt-get', 'remove', 'nohang', '-y'])
+    pro5 = subprocess.run(['sudo', 'apt-get', 'remove', 'fish', '-y'])
+    pro6 = subprocess.run(['sudo', 'apt-get', 'remove', 'mesa-utils', '-y'])
+    pro7 = subprocess.run(['sudo', 'apt-get', 'remove', 'mesa-utils-extra', '-y'])
+    pro8 = subprocess.run(['sudo', 'apt-get', 'remove', 'indicator-cpufreq', '-y'])
+    pro9 = subprocess.run(['sudo', 'apt-get', 'remove', 'profile-sync-daemon', '-y'])
+    pro10 = subprocess.run(['sudo', 'apt-get', 'remove', 'fakeroot', '-y'])
+    pro11 = subprocess.run(['sudo', 'systemctl', 'disable', '--now', 'prelockd.service'])
+    pro12 = subprocess.run(['sudo', 'apt-get', 'remove', 'zram-config', '-y'])
+    pro13 = subprocess.run(['sudo', 'apt-get', 'remove', 'tlp', '-y'])
+    pro14 = subprocess.run(['sudo', 'systemctl', 'disable', '--now', 'memavaild.service'])
+    pro15 = subprocess.run(['sudo', 'apt-get', 'remove', 'tuned', 'tuned-utils', 'tuned-utils-systemtap', '-y'])
+    pro16 = subprocess.run(['sudo', 'apt-get', 'remove', 'fail2ban', '-y'])
+    pro17 = subprocess.run(['sudo', 'ufw', 'disable'])
+    pro18 = subprocess.run(['sudo', 'apt-get', 'remove', 'git', '-y'])
+    pro19 = subprocess.run(['sudo', 'systemctl', 'disable', 'ananicy'])
 
 
 
+    print(pro.returncode)
     print(pro2.returncode)
     print(pro3.returncode)
     print(pro4.returncode)
@@ -93,20 +131,30 @@ def unistallingaos_commands():
     print(pro17.returncode)
     print(pro18.returncode)
     print(pro19.returncode)
-    print(pro20.returncode)
-    print(pro21.returncode)
 
 
-    if int(pro2.returncode + pro3.returncode + pro4.returncode + pro5.returncode + pro6.returncode + pro7.returncode + 
+    if int(pro.returncode + pro2.returncode + pro3.returncode + pro4.returncode + pro5.returncode + pro6.returncode + pro7.returncode + 
        pro8.returncode + pro9.returncode + pro10.returncode + pro11.returncode + pro12.returncode + pro12.returncode + pro14.returncode + 
-       pro15.returncode + pro16.returncode + pro17.returncode + pro18.returncode + pro19.returncode + pro20.returncode + pro21.returncode)==0:
+       pro15.returncode + pro16.returncode + pro17.returncode + pro18.returncode + pro19.returncode)==0:
        print("##########################################################") 
        print("*             unstalling aos was successful              *")
        print("##########################################################")
        print("")
        print("the program will continue the installation process in a few seconds, please wait ...")
-       time.sleep(5)
-            
+       time.sleep(3)
+
+    else:
+
+       print("#####################################################################") 
+       print("*                warning: unstalling aos was failed                 *")
+       print("#####################################################################")
+       time.sleep(3)
+       print("")
+       print("")
+       loop = input("Do you want to try to unstalling aos again? [y/n]")  
+       if loop  == "y":
+        subprocess.run(['sudo', 'bash', 'scripts/fix.sh'])
+        unistalling_commands()                
 
   unistalling_commands()  
 
@@ -123,21 +171,43 @@ def unistallingaos_commands():
     pro = subprocess.run(['sudo', 'apt-get', 'install', 'update-notifier', '-y'])
     pro2 = subprocess.run(['sudo', 'apt-get', 'install', 'thunderbird', '-y'])
     pro3 = subprocess.run(['sudo', 'apt-get', 'install', 'gnome-software', '-y'])
+    pro4 = subprocess.run(['sudo', 'apt-get', 'install', 'bluez', '-y'])
+    pro5 = subprocess.run(['sudo', 'apt-get', 'install', 'xscreensaver', '-y'])
+#### return snap if is removed 
+    pro6 = subprocess.run(['sudo', 'apt-get', 'install', 'snap', '-y'])    
+    pro7 = subprocess.run(['sudo', 'apt-get', 'install', 'snapd', '-y'])
 
 
     print(pro.returncode)
     print(pro2.returncode)
     print(pro3.returncode)
+    print(pro4.returncode)
+    print(pro5.returncode)
+    print(pro6.returncode)
+    print(pro7.returncode)
 
- 
-    if int(pro.returncode + pro2.returncode + pro3.returncode)==0:
+    if int(pro.returncode + pro2.returncode + pro3.returncode + pro5.returncode + pro5.returncode +
+       pro6.returncode + pro7.returncode)==0:
        print("#########################################################") 
        print("*        return unnecessary tools was successful        *")
        print("#########################################################")
        print("")
        print("the program will continue the installation process in a few seconds, please wait ...")
        time.sleep(3)
-            
+
+    else:
+
+       print("#########################################################################") 
+       print("*              warning: return unnecessary tools was failed             *")
+       print("#########################################################################")
+       time.sleep(3)
+       print("")
+       print("")
+       loop = input("Do you want to try to return unnecessary tools again? [y/n]")  
+       if loop  == "y":
+        subprocess.run(['sudo', 'bash', 'scripts/fix.sh'])
+        reunnecessary_commands()        
+        
 
   reunnecessary_commands()  
 
@@ -158,23 +228,33 @@ def unistallingaos_commands():
     pro3 = subprocess.run(['sudo', 'add-apt-repository', '--remove', 'ppa:linuxuprising/apps', '-yy'])
     pro4 = subprocess.run(['sudo', 'add-apt-repository', '--remove', 'ppa:system76/pop', '-yy'])
     pro5 = subprocess.run(['sudo', 'add-apt-repository', '--remove', 'ppa:fish-shell/release-3', '-yy'])
-    pro6 = subprocess.run(['sudo', 'add-apt-repository', '--remove', 'ppa:system76/pop', '-yy'])
 
     print(pro.returncode)
     print(pro2.returncode)
     print(pro3.returncode)
     print(pro4.returncode)
     print(pro5.returncode)
-    print(pro6.returncode)
 
-    if int(pro.returncode + pro2.returncode + pro3.returncode + pro4.returncode + pro5.returncode + pro6.returncode)==0:
+    if int(pro.returncode + pro2.returncode + pro3.returncode + pro4.returncode + pro5.returncode)==0:
        print("############################################################") 
        print("*             unstalling aos_ppa was successful            *")
        print("############################################################")
        print("")
        print("the program will continue the installation process in a few seconds, please wait ...")
-       time.sleep(5)
-            
+       time.sleep(3)
+
+    else:
+
+       print("#########################################################################") 
+       print("*                warning: unstalling aos_ppa was failed                 *")
+       print("#########################################################################")
+       time.sleep(3)
+       print("")
+       print("")
+       loop = input("Do you want to try to unstalling aos_ppa again? [y/n]")  
+       if loop  == "y":
+        subprocess.run(['sudo', 'bash', 'scripts/fix.sh'])
+        reppa_commands()        
 
   reppa_commands()  
 
@@ -220,6 +300,7 @@ def unistallingaos_commands():
        print("")
        loop = input("Do you want to try to return important files again? [y/n]")  
        if loop  == "y":
+        subprocess.run(['sudo', 'bash', 'scripts/fix.sh'])
         returnup_commands()
             
 
@@ -239,11 +320,10 @@ def unistallingaos_commands():
 ######### check if firefox is installed
     pro = subprocess.run(['sudo', 'apt-get', 'update']) 
     pro2 = subprocess.run(['sudo', 'apt-get', 'install', 'firefox', '-y'])
-
-    print(pro.returncode)
-    print(pro2.returncode)
-
-    if int(pro.returncode + pro2.returncode)==0:
+    pro3 = subprocess.run(['apt-cache', 'policy', 'firefox'])
+    print(pro3.returncode)
+    
+    if int(pro3.returncode)==0:
         print("#########################################################") 
         print("*          firefox is installed in the system           *")
         print("#########################################################")
@@ -268,16 +348,108 @@ def unistallingaos_commands():
         print("If you choose, *no* ,the program will continue without removing chromium")
         print("so that there will be no situation where you will be left without a browser.")
         print("")
-        loop = input("Do you want to try to installing firefox again before you remove chromium [y/n] ?")
+        loop = input("Do you want to try installing firefox before you remove chromium [y/n] ?")
         if loop  == "y":
+          subprocess.run(['sudo', 'bash', 'scripts/fix.sh'])
           rmchromium_commands()
 
 
   rmchromium_commands()
 
 
+####################
+# apparmor disable #
+####################
+
+
+  def diapparmor_commands():
+####### chek if apparmor enable
+   pro0 = subprocess.run(['aa-enabled'])
+   print(pro0.returncode)
+   if int(pro0.returncode)==0:
+    print("")
+    print("")
+    print("The program has identified apparmor is enable")
+    print("")
+    disapparmor = input("Do you want to disable apparmor ? [y/n] ")  
+    if disapparmor  == "y":
+
+      pro = subprocess.run(['sudo', 'bash', 'scripts/apparmor_disable.sh'])
+      pro2 = subprocess.run(['sudo', 'apt-get', 'remove', 'apparmor-easyprof', 'apparmor-notify', 'apparmor-utils', 'certspotter', '-y'])
+      pro3 = subprocess.run(['sudo', 'update-grub'])
+
+      print(pro.returncode)
+      print(pro2.returncode)
+      print(pro3.returncode)
+      
+
+      if int(pro.returncode + pro2.returncode + pro3.returncode)==0:
+          print("#######################################################") 
+          print("*            Successfully disable apparmor            *")
+          print("#######################################################")
+          print("")
+          print("")
+          print("the program will continue the installation process in a few seconds, please wait ...")
+          time.sleep(3)
+
+      else:
+
+          print("##########################################################################") 
+          print("*                      Failed to disable apparmor                        *")
+          print("##########################################################################")
+          time.sleep(3)
+          print("")
+          loop = input("Do you want to try to disable apparmor again ? [y/n]")  
+          if loop  == "y":
+            subprocess.run(['sudo', 'bash', 'scripts/fix.sh'])
+            diapparmor_commands()
+
+  diapparmor_commands()
+
+
+#############################
+#  autoclean && autoremove  #
+#############################
+
+
+  def autoclean_commands():
+    pro = subprocess.run(['sudo', 'apt-get', 'autoclean', '-y'])
+    pro2 = subprocess.run(['sudo', 'apt-get', 'auto-remove', '-y'])
+
+    print(pro.returncode)
+    print(pro2.returncode)
+
+    if int(pro.returncode + pro2.returncode)==0:
+       print("###########################################################") 
+       print("*         autoclean && autoremove was successful          *")
+       print("###########################################################")
+       print("")
+       print("the program will continue the installation process in a few seconds, please wait ...")
+       time.sleep(3)
+
+    else:
+
+       print("##############################################################################") 
+       print("*                warning: autoclean && autoremove was failed                 *")
+       print("##############################################################################")
+       time.sleep(3)
+       print("")
+       print("")
+       loop = input("Do you want to try to autoclean && autoremove again? [y/n]")  
+       if loop  == "y":
+        subprocess.run(['sudo', 'bash', 'scripts/fix.sh'])
+        autoclean_commands()
+            
+
+  autoclean_commands()
+
+
 unistallingaos_commands()
 
 print("")
-print("Uninstallation complete.please reboot the system")
+print("For more details")
+print("#########################################")
+print("https://github.com/ramner98/LINUX-AOS.git")
+print("#########################################")
+time.sleep(3)
 print("")
